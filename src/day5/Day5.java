816084d689731eca68650e8ad913ca66e7bcc9f1
Shahman Magomadov <magomadovshahman@gmail.com>
2022-12-21 11:46:31 +0300

    start day6

--- src/day5/Day5.java
+++ src/day5/Day5.java
@@ -1,9 +1,11 @@
+package day5;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-public class NewMain {
+public class Day5 {
 	public static void main(String[] args) throws IOException {
 		String path = "src/test.txt";
 		String line = "";

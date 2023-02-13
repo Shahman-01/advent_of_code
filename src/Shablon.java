032ab1670553d1e8fec195fbd0c1b44d8421a28a
Shahman Magomadov <magomadovshahman@gmail.com>
2023-02-13 18:06:33 +0300

    did day6

--- src/Shablon.java
+++ src/Shablon.java
@@ -1,8 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Shablon {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader("src/day6/test"));
 	}
 }
--- src/day6/Day6.java
+++ src/day6/Day6.java
@@ -1,5 +1,31 @@
 package day6;
 
-public class Day6 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Day6 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader("src/day6/test"));
+		System.out.println(f(bufferedReader));
+	}
+
+	public static int f(BufferedReader reader) throws IOException {
+		char[] arr = reader.readLine().toCharArray();
+		for (int i = 0; i < arr.length; i++) {
+			if (i < arr.length - 5 && check(arr[i], arr[i + 1], arr[i + 2], arr[i + 3]))
+				return i + 4;
+		}
+		return 0;
+	}
+
+	public static boolean check(char x1, char x2, char x3, char x4) {
+		if (x1 == x2 || x1 == x3 || x1 == x4)
+			return false;
+		if (x2 == x3 || x2 == x4)
+			return false;
+		if (x3 == x4)
+			return false;
+		return true;
+	}
 }
